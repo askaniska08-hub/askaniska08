eb19d8470d352fcb435f1514f6595009889070a6
A.S. Kaniska <askaniska08@gmail.com>
2026-06-12 15:50:51 +0530

    Add multiplication output to Division.py

--- Division.py
+++ Division.py
@@ -1,3 +1,4 @@
 num1=int(input("Enter your first number"))
 num2=float(input("Enter your second number"))
 print("Division of two numbers is:", num1/num2)
+print("Multiplication of the two number is:", num1*num2)
